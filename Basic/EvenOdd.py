35faa3fa8cae041452461c092e9fed3524c44830
RajShashwat <shashwat.r2018bcs@srisriuniversity.edu.in>
2019-02-18 15:48:05 +0530

    A program to check a no is even or odd

---
+++ Basic/EvenOdd.py
@@ -0,0 +1,8 @@
+#Created by Shashwat Raj
+#Program to check a no is even or odd
+num=int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even Number");
+else:
+    print("Odd Number")
+        
