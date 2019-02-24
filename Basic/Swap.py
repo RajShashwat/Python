6adb589edd583af40678505b921e0f8f4785d1ed
RajShashwat <shashwat.r2018bcs@srisriuniversity.edu.in>
2019-02-24 16:14:58 +0530

    A program to swap the numbers

---
+++ Basic/Swap.py
@@ -0,0 +1,8 @@
+#Created by Shashwat Raj
+#Program to swap 2 nos
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the Second number: "))
+temp=num1
+num1=num2
+num2=temp
+print("Numbers after swap:\nFirst Number: ",num1,"\nSecond Number: ",num2)
