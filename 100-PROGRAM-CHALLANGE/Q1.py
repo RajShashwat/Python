f77d0133e65cfe22da492d7a8c1223eafbefed8a
Shashwat Raj <shashwat.r2018bcs@srisriuniversity.edu.in>
2019-10-10 21:15:15 +0530

    Program1

--- 100-PROGRAM-CHALLANGE/Q1.py
+++ 100-PROGRAM-CHALLANGE/Q1.py
@@ -1,14 +1,14 @@
-'''
-Question:
-Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
-between 2000 and 3200 (both included).
+# Solved by Shashwat Raj
+"""
+Write a program which will find all such numbers which are divisible by 3 and 5 but are not a multiple of 7,
+between 7700 and 15899 (both included).
 The numbers obtained should be printed in a comma-separated sequence on a single line.
-'''
+"""
 
-#Solved by Shashwat Raj
-
-u=2000
-v=3200
-for i in range(u,v):
-    if i%7==0 and i%5!=0:
-        print(i, end=',')
+n=[]
+u=7700
+v=15900
+for i in range(u , v):
+    if i % 3 == 0 and i % 5 == 0 and i % 7 != 0 :
+        n.append(str(i))
+print (','.join(n))
